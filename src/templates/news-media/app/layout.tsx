883de3469c99a88a777d68fbca263b68d8adf218
yanas-logs <yanas.dev404@gmail.com>
2025-12-06 23:58:43 +0700

    feat(components): create and apply HeadLine component

--- src/templates/news-media/app/layout.tsx
+++ src/templates/news-media/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import MastHead from "@/components/MastHead";
+import HeadLine from "@/components/HeadLine";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <HeadLine />
         <MastHead />
         <Navbar />
         {children}
--- src/templates/news-media/components/HeadLine.tsx
+++ src/templates/news-media/components/HeadLine.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-export default function MastheadTitle() {
+export default function Headline() {
   return (
     <div // Main
       style={{
